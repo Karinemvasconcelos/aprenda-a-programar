4d7f36c3089259a0fad4e45c54cc165f726d18d7
Karine Vasconcelos <karine.mv.98@gmail.com>
2022-08-26 09:25:32 -0300

    tarefa-dia-2 atualizada

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,10 @@
-let SeuNome = "Karine Vasconcelos"
-let SeuEmail = "karine.mv.98@gmail.com"
-let SuaProfissao = "Administradora"
-let SuaIdade = 23
-let SeuSonho = "Italia"
+let SeuNome
+SeuNome = "Karine Vasconcelos"
+let SeuEmail 
+SeuEmail = "karine.mv.98@gmail.com"
+let SuaProfissao
+SuaProfissao = "Administradora"
+let SuaIdade
+SuaIdade = "23"
+let SeuSonho
+SeuSonho = "Italia"
